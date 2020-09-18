f8c3f9efcf3667d7add4261b0f4c93d60f7d1b55
Asis Perez <jose.perez2882@alumnos.udg.mx>
2020-09-18 16:49:13 -0500

    Nombre completo capturado con fgets

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 
 int main(){
+    char name[100];
     printf("Hola mundo\n");
-
+    printf("Nombre: ");
+    fgets(name, sizeof(name), stdin);
+    printf("Hola %s",name);
     return 0;
 }
