681d9484c1213332c026d0e7aad6dba09c5c8904
Asis Perez <jose.perez2882@alumnos.udg.mx>
2020-09-20 10:56:51 -0500

    Nos pide el mes e indica si somos menores o mayores de edad

--- edad.c
+++ edad.c
@@ -5,6 +5,16 @@ int main(){
     int year, month, age;
     printf("¿Cuál es tu año de nacimiento?");
     scanf("%d",&year);
+    printf("¿Cuál es tu mes (número) de nacimiento?");
+    scanf("%d",&month);
     age = 2020-year;
     printf("Tu edad es: %d",age);
+    //
+    if(age<18 && month >9){
+        printf("Es menor de edad\n");
+    }else{
+        printf("Es mayor de edad\n");
+    }
+
+    return 0;
 }
